15dbb0249bd513c3f0622cbd7ebb73092a1d155f
Strawberryhalo <j.waites992@gmail.com>
2025-09-08 22:58:21 -0400

    Update Movement.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Movement.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Movement.java
@@ -10,7 +10,6 @@ import com.qualcomm.robotcore.hardware.DcMotorSimple;
 @TeleOp
 public class Movement extends LinearOpMode {
     //define each wheel/control hub as variables
-    //edit test
     private Blinker control_hub;
     private DcMotor RBwheel;
     private DcMotor RFwheel;
